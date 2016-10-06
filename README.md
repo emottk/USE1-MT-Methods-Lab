1af8d68ba824dcfa91bafb09aae75513fa59ad53
emottk <emily.kowal@postlight.com>
2016-10-06 16:56:37 -0400

    lab and readme updates

---
+++ README.md
@@ -0,0 +1,3 @@
+#Methods
+
+Follow the instructions in `lab.rb` to creat your very own methods!
---
+++ lab.rb
@@ -0,0 +1,45 @@
+
+def say_hello!
+  #puts "hello!"
+end
+
+#write a method that converts years into months
+def years_to_months(years)
+  #some code here
+end
+
+
+#write a method that will take in a person's name and greet them
+def greeter(name)
+  #some code here
+end
+
+
+#write a method that will take in an age an turn it into dog years
+def
+  #some code here
+end
+
+
+#write any method you'd like that takes in and uses two values
+def
+  #some code here
+end
+
+
+#write a method that uses the years_to_months method inside of it
+def
+  #some code here
+end
+
+
+#write a method that says "time for a new toothbrush" if the input is greater than 2.
+def
+  #some code here
+end
+
+
+#call all of your methods below, and then run this program in your terminal!
+say_hello!
+years_to_months(34)
+greeter("Cynthia")
